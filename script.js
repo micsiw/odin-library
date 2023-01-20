d659523e82bdd727875f1e04c34402c59545bc2a
micsiw <michalsiwanowicz@gmail.com>
2023-01-20 18:23:01 +0100

    added remove button to delete books from screen and array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addBookButton = document.querySelector('[data-open-modal');
 const closeAddBook = document.querySelector('[data-close-modal');
+const addToLibrary = document.querySelector('[data-add-to-library');
 const mainBody = document.querySelector('[data-main-body');
 const modal = document.querySelector('[data-modal]');
 const overlay = document.getElementById('overlay');
@@ -8,6 +9,8 @@ const frankenstein = new Book('Frankenstein', 'Shelley', '269', '1818', 'yes');
 const twilight = new Book('Twilight', 'Meyer', '434', '2007', 'no')
 
 let myLibrary = [frankenstein, twilight];
+
+displayLibrary();
 
 function Book(title, author, pages, release, read) {
     this.title = title,
@@ -20,53 +23,93 @@ function Book(title, author, pages, release, read) {
 
 function addBookToLibrary(title, author, pages, release, read) {
 
-    const newBook = new Book(title, author, pages, release, read)
-
-    myLibrary.push(newBook)
+    myLibrary.push(new Book(title, author, pages, release, read))
 
 }
 
-myLibrary.forEach((book) => {
+function displayLibrary() {
 
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('library-card');
-    mainBody.appendChild(newDiv);
+    mainBody.innerHTML = '';
 
-    const newTitle = document.createElement('p');
-    newTitle.textContent = book.title;
-    newTitle.classList.add('card-title');
-    newDiv.appendChild(newTitle)
+    myLibrary.forEach((book, index) => {
 
-    const newAuthor = document.createElement('p');
-    newAuthor.textContent = book.author;
-    newAuthor.classList.add('card-author');
-    newDiv.appendChild(newAuthor)
+        const newDiv = document.createElement('div');
+        newDiv.dataset.indexNumber = index;
+        newDiv.classList.add('library-card');
+        mainBody.appendChild(newDiv);
 
-    const newPages = document.createElement('p');
-    newPages.textContent = book.pages;
-    newPages.classList.add('card-pages');
-    newDiv.appendChild(newPages)
+        const newTitle = document.createElement('p');
+        newTitle.textContent = book.title;
+        newTitle.classList.add('card-title');
+        newDiv.appendChild(newTitle)
 
-    const newRelease = document.createElement('p');
-    newRelease.textContent = book.release;
-    newRelease.classList.add('card-release');
-    newDiv.appendChild(newRelease)
+        const newAuthor = document.createElement('p');
+        newAuthor.textContent = book.author;
+        newAuthor.classList.add('card-author');
+        newDiv.appendChild(newAuthor)
 
-    const newRead = document.createElement('p');
-    newRead.textContent = 'Have I read already?';
-    newRead.classList.add('card-release');
-    newDiv.appendChild(newRead)
+        const newPages = document.createElement('p');
+        newPages.textContent = book.pages;
+        newPages.classList.add('card-pages');
+        newDiv.appendChild(newPages)
 
+        const newRelease = document.createElement('p');
+        newRelease.textContent = book.release;
+        newRelease.classList.add('card-release');
+        newDiv.appendChild(newRelease)
+
+        const newRead = document.createElement('p');
+        newRead.textContent = 'Have I read already?';
+        newRead.classList.add('card-read');
+        newDiv.appendChild(newRead)
+
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        newRead.classList.add('remove-button');
+        newDiv.appendChild(removeButton)
+
+        removeButton.addEventListener('click', () => {
+            const cardToRemoveIndex = newDiv.dataset.indexNumber;
+            myLibrary.splice(cardToRemoveIndex, 1);
+            displayLibrary();
+        })
+
+    })
+
+}
+
+//?
+Book.prototype.didRead = () => {
+    this.read = 'yes';
+}
+
+addToLibrary.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    const title = document.getElementById('name').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const release = document.getElementById('release').value;
+    const read = document.getElementById('read').value;
+
+    addBookToLibrary(title, author, pages, release, read);
+
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+
+    addToLibrary.reset();
+    displayLibrary();
 })
 
 addBookButton.addEventListener('click', () => {
-    modal.classList.add('active')
-    overlay.classList.add('active')
+    modal.classList.add('active');
+    overlay.classList.add('active');
 })
 
 closeAddBook.addEventListener('click', () => {
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
 })
 
 
